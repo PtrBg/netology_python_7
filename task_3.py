b6b661ea6a70f84ccbf17b1f7d488644a0991e8d
Petr Bogov <petrbogov@192.168.1.16>
2023-01-06 19:21:15 +0300

    исправлено задание 3

--- task_3.py
+++ task_3.py
@@ -1,13 +1,21 @@
 import os
 
 file_path = os.path.join(os.getcwd(), 'sorted')
-result_file = open(os.path.join(file_path, 'result.txt'), 'wt', encoding='utf-8')
+files = {}
 
 for file_name in os.listdir(file_path):
-    if file_name.endswith(".txt") and file_name != 'result.txt':
-        with open(os.path.join(file_path, file_name), 'rt', encoding='utf-8') as file:
-            file_content = file.readlines()
-            result_file.write(f'{file_name}\n')
-            result_file.write(f'{str(len(file_content))}\n')
-            result_file.writelines(file_content)
+	if file_name.endswith(".txt") and file_name != 'result.txt':
+		with open(os.path.join(file_path, file_name), 'rt', encoding='utf-8') as file:
+			file_content = file.readlines()
+			files[file_name] = {'size': len(file_content),
+								'content': file_content}
+
+files_sorted = dict(sorted(files.items(), key=lambda item: item[1]['size']))
+result_file = open(os.path.join(file_path, 'result.txt'), 'wt', encoding='utf-8')
+
+for file, file_info in files_sorted.items():
+	result_file.write(f'{file}\n')
+	result_file.write(f'{file_info["size"]}\n')
+	result_file.writelines(file_info["content"])
 result_file.close()
+
